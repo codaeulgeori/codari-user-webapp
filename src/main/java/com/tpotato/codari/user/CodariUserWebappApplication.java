5c7368d4186b1cd027c0a502d40e0360ec72753b
jhpark-mac <ggoowlgns@gmail.com>
2022-07-30 18:52:57 +0900

    first msg

---
+++ src/main/java/com/tpotato/codari/user/CodariUserWebappApplication.java
@@ -0,0 +1,13 @@
+package com.tpotato.codari.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodariUserWebappApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CodariUserWebappApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/tpotato/codari/user/CodariUserWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpotato.codari.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodariUserWebappApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
